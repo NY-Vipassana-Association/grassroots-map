702eebdec94b5a0efcf4fc6a39f80007910ddcf9
Nathan Shane <nathan.shane@flocabulary.com>
2018-08-07 20:03:19 -0400

    Center map in new york

---
+++ src/index.js
@@ -0,0 +1,14 @@
+var mymap = L.map("mapid").setView([40.7128, -74.006], 13);
+L.tileLayer(
+  "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}",
+  {
+    attribution:
+      'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: "mapbox.streets",
+    accessToken:
+      "pk.eyJ1IjoibmF0YW5pYmFyIiwiYSI6ImNqa2FnMTM5ajM1ajYzbG50dXptMDhjcDIifQ.Dae3BHZd9sexPOk_d76O1g"
+  }
+).addTo(mymap);
+
+var marker = L.marker([40.7128, -74.006]).addTo(mymap);
