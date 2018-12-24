c86dc078d34e7a8921dc1bff12efa7bab60178c0
Nathan Shane <nathan.shane@flocabulary.com>
2018-12-24 10:54:04 -0500

    Add popup to region layer

--- src/components/RegionLayer.tsx
+++ src/components/RegionLayer.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { GeoJSON } from "react-leaflet";
+import { GeoJSON, Popup } from "react-leaflet";
 
 import nycBoroughsJSON from "../nycBoroughs.json";
 import { IRegionFeatureCollection } from "../types";
@@ -8,6 +8,19 @@ const nycBoroughsData = nycBoroughsJSON as IRegionFeatureCollection;
 
 export default class RegionGeoJSONLayer extends React.Component {
   render() {
-    return <GeoJSON data={nycBoroughsData} />;
+    return (
+      <GeoJSON data={nycBoroughsData}>
+        <Popup>
+          <p>
+            <a
+              href="https://docs.google.com/document/d/1Q3S9qwr1akRhVcNKRcnCA7wWCQiMayVLTcoVwzCGBo4/edit"
+              target="_blank"
+            >
+              Apply to host a group sitting
+            </a>
+          </p>
+        </Popup>
+      </GeoJSON>
+    );
   }
 }
