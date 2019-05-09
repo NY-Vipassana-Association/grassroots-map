7df2141606740d8660178a55af8abb50740d6300
Nathan Shane <nathan.shane@flocabulary.com>
2019-05-09 19:47:08 -0400

    Get student counts from separate spreadsheet worksheet

--- src/scripts/fetch-google-sheets-data.js
+++ src/scripts/fetch-google-sheets-data.js
@@ -1,22 +1,60 @@
+// @ts-ignore
 const gsjson = require("google-spreadsheet-to-json");
 const fs = require("fs");
+
+// Inserts the old student counts from the all_county_student_counts worksheet
+// into the map counties worksheet
+const includeOldStudentCounts = (
+  mapCounties,
+  allCountyStudentCountsResponse
+) => {
+  return mapCounties.map(mapCounty => {
+    const matchingCountyWithStudentCount = allCountyStudentCountsResponse.find(
+      countyWithStudentCount =>
+        countyWithStudentCount.state === mapCounty.state_name &&
+        (countyWithStudentCount.county === mapCounty.region_name ||
+          countyWithStudentCount.county === mapCounty.official_name)
+    );
+
+    if (!matchingCountyWithStudentCount) {
+      console.error(
+        `Could not find matching county for ${mapCounty.region_name}, ${
+          mapCounty.state_name
+        }`
+      );
+
+      return 0;
+    }
+
+    return {
+      ...mapCounty,
+      student_count_all_time: matchingCountyWithStudentCount.osCount
+    };
+  });
+};
 
 gsjson({
   spreadsheetId: "15bAkyPI-hJj8N0sQGU3f47CCvClewkx6YQAJ-ASC_M4",
   allWorksheets: true
 })
   .then(function(result) {
-    const student_counts_response = result[0];
-    const group_sittings_response = result[1];
+    const studentCountsResponse = result[0];
+    const groupSittingsResponse = result[1];
+    const allCountyStudentCountsResponse = result[2];
 
     fs.writeFileSync(
       "./src/data/gitignored/oldStudentData.json",
-      JSON.stringify(student_counts_response)
+      JSON.stringify(
+        includeOldStudentCounts(
+          studentCountsResponse,
+          allCountyStudentCountsResponse
+        )
+      )
     );
 
     fs.writeFileSync(
       "./src/data/gitignored/groupSittings.json",
-      JSON.stringify(group_sittings_response)
+      JSON.stringify(groupSittingsResponse)
     );
   })
   .catch(function(err) {
